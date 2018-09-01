3b53b93382cab3c57a60dff977a612c18acd3236
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2018-09-01 01:55:27 -0700

    End of first day.

---
+++ Project/GMTK Jam 2018/Assets/Scripts/Distraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Distraction : MonoBehaviour
+{
+	public enum DistractionType
+	{
+		Smell,
+		Noise
+	}
+	public DistractionType distractionType;
+
+	public float distractionAmount = 10.0f;
+	
+	public AnimationCurve falloff = AnimationCurve.EaseInOut(0, 1, 1, 0);
+
+	public float radius;
+
+	private void Awake()
+	{
+		radius = GetComponent<CircleCollider2D>().bounds.extents.x;
+	}
+}
--- Project/GMTK Jam 2018/Assets/Scripts/Player.cs
+++ Project/GMTK Jam 2018/Assets/Scripts/Player.cs
@@ -7,17 +7,17 @@ public class Player : MonoBehaviour
 	//Components & References------------------------------------------------------------------------------------------/
 	private Rigidbody2D m_Rig;
 	
-	//Values
+	//Values-----------------------------------------------------------------------------------------------------------/
 	[SerializeField] private bool m_IsMoving;
 	
-	//Stats
+	
+	//Stats------------------------------------------------------------------------------------------------------------/
+	//Player
 	[SerializeField] private float m_MoveSpeed = 1;
 	[SerializeField] private float m_IdleFartSpeed = 1;
 	[SerializeField] private float m_MovingFartSpeed = 1;
-	
-	[Space]
-	
 	[SerializeField] private float m_FartMax = 100;
+	[SerializeField] private float m_CloutMax = 100;
 	private float m_FartCurrent;
 	public float Fart
 	{
@@ -30,9 +30,6 @@ public class Player : MonoBehaviour
 			m_FartCurrent = value;
 		}
 	}
-
-	[Space]
-	[SerializeField] private float m_CloutMax = 100;
 	private float m_CloutCurrent;
 	public float Clout
 	{
@@ -46,7 +43,37 @@ public class Player : MonoBehaviour
 		}
 	}
 	
+	//Environment
+	[SerializeField] private float m_StenchMax = 100;
+	private float m_StinkCurrent;
+	[SerializeField] private float m_NoiseMax = 100;
+	private float m_NoiseCurrent;
+	
+	//Farts
+	private enum FartSmell
+	{
+		Unsmellable,
+		Smelly,
+		Putrid
+	}
+	private FartSmell m_FartSmell;
 
+	private enum FartNoise
+	{
+		Silent,
+		Noticable,
+		Loud
+	}
+	private FartNoise m_FartNoise;
+
+	private enum FartWetness
+	{
+		Dry,
+		Moist,
+		Wet
+	}
+	private FartWetness m_FartWetness;
+	
 	private void Awake()
 	{
 		//Assign References
@@ -56,11 +83,17 @@ public class Player : MonoBehaviour
 		m_CloutCurrent = m_CloutMax;
 	}
 
+	private void Start()
+	{
+		NextFart();
+	}
+
 	private void Update()
 	{
 		PlayerInput();
 		UpdateUI();
 		FartBuildup();
+		EnvironmentDetection();
 	}
 
 	private void PlayerInput()
@@ -75,6 +108,8 @@ public class Player : MonoBehaviour
 	{
 		UI_Gameplay.instance.FartMeter = m_FartCurrent / m_FartMax;
 		UI_Gameplay.instance.CloutMeter = m_CloutCurrent / m_CloutMax;
+		UI_Gameplay.instance.StenchMeter = m_StinkCurrent / m_StenchMax;
+		UI_Gameplay.instance.NoiseMeter = m_NoiseCurrent / m_NoiseMax;
 	}
 
 	private void FartBuildup()
@@ -84,15 +119,66 @@ public class Player : MonoBehaviour
 
 		if (m_FartCurrent >= m_FartMax)
 		{
-			OnFart();
+			StartCoroutine(OnFart());
 		}
 		
 	}
 
-	private void OnFart()
+	private IEnumerator OnFart()
 	{
-		m_FartCurrent = 0.0f;
+		float f = Fart;
+		float timer = 0.5f;
+		float t = 0.0f;
+		while (t < timer)
+		{
+			t += Time.deltaTime;
+
+			Fart = Mathf.Lerp(f, 0, t / timer);
+			
+			yield return null;
+		}
 		
-		Debug.Log("FART");
+		NextFart();
+	}
+
+	private void NextFart()
+	{
+		m_FartSmell = (FartSmell) Random.Range(0, System.Enum.GetValues(typeof(FartSmell)).Length);
+		m_FartNoise = (FartNoise) Random.Range(0, System.Enum.GetValues(typeof(FartNoise)).Length);
+		m_FartWetness = (FartWetness) Random.Range(0, System.Enum.GetValues(typeof(FartWetness)).Length);
+
+		UI_Gameplay.instance.NextFart = "A " + m_FartSmell + ", " + m_FartNoise + ", but " + m_FartWetness + " fart.";
+	}
+
+	private void EnvironmentDetection()
+	{
+		float stench = 0;
+		float noise = 0;
+		
+		RaycastHit2D[] env = Physics2D.CircleCastAll(transform.position, 0.5f, Vector2.up, 1.0f, 1 << LayerMask.NameToLayer("Environment"));
+		foreach (RaycastHit2D obj in env)
+		{
+			Distraction distraction = obj.transform.GetComponent<Distraction>();
+			if (distraction != null)
+			{
+				float dist = Vector2.Distance(transform.position, distraction.transform.position);
+				float falloff = distraction.falloff.Evaluate(dist / distraction.radius );
+				float output = distraction.distractionAmount * falloff;
+				
+				switch (distraction.distractionType)
+				{
+						case Distraction.DistractionType.Smell:
+							stench += output;
+							break;
+						
+						case Distraction.DistractionType.Noise:
+							noise += output;
+							break;
+				}
+			}
+		}
+
+		m_StinkCurrent = stench;
+		m_NoiseCurrent = noise;
 	}
 }

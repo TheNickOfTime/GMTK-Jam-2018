d91f1b1cf2d5da1c5e08795d5663aafe4962af40
Nick Cunningham <thenickoftime.x@gmail.com>
2018-09-01 14:10:17 -0700

    Upload for Garry

---
+++ Project/GMTK Jam 2018/Assets/Scripts/Hazard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+	private const float decayRate = 1;
+	
+	public float smell;
+	public float noise;
+
+	private void Update()
+	{
+		smell -= decayRate * Time.deltaTime;
+		smell = Mathf.Clamp(smell, 0, 100);
+
+		noise -= decayRate * Time.deltaTime;
+		noise = Mathf.Clamp(noise, 0, 100);
+
+		bool destroy = smell <= 0 && noise <= 0 ? true : false;
+		if (destroy)
+		{
+			Destroy(gameObject);
+		}
+	}
+}

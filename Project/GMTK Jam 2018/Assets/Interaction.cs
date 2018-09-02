6bf99e692469d3c11b873430d7aa9d4d808c7cdc
Aeuron <garrymcgee96@gmail.com>
2018-09-01 17:55:14 -0700

    Changes
    +NPC
    +Interactions
    +New Scene
    +Added stuff to level

---
+++ Project/GMTK Jam 2018/Assets/Interaction.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject plate, trash, plant, dog, stereo;
+    private void Start()
+    {
+        plate.SetActive(false);
+        trash.SetActive(false);
+        plant.SetActive(false);
+        dog.SetActive(false);
+        stereo.SetActive(false);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Plate")
+        {
+            plate.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("I've broken a plate!");
+            }
+                       
+        }
+        else
+        {
+            plate.SetActive(false);
+        }
+        if (collision.gameObject.tag == "Trash" )
+        {
+            trash.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("I farted near the trashcan.");
+            }
+        }
+        else
+        {
+            trash.SetActive(false);
+        }
+        if (collision.gameObject.tag == "Plant")
+        {
+            plant.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("I've pooped in the plant!");            
+            }
+        }
+        else
+        {
+            plant.SetActive(false);
+        }
+        if (collision.gameObject.tag == "Dog")
+        {
+            dog.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("I'm blaming my farts on Spike here.");            
+            }
+        }
+        else
+        {
+            dog.SetActive(false);
+        }
+        if (collision.gameObject.tag == "Stereo")
+        {
+            stereo.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Turning the music louder so I can let out some farts.");            
+            }
+        }
+        else
+        {
+            stereo.SetActive(false);
+        }
+    }
+}
